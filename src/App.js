819b15c08aed26b30c77965ffd78a36008e30c80
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-06-01 22:38:31 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+
+
+  const [count, setCount]=  useState(0);
+  function decreaseHandler(){
+   setCount(count-1) 
+  }
+
+  function increaseHandler(){
+    setCount(count+1)
+    }
+  function resetHandler(){
+    setCount(0);
+  }
   return (
-    <div>
-      Hello ji
+   <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col">
+
+    <div className="text-center text-[#0398d4] font-medium text-3xl m-7">Increment && decrement</div>
+    <div className="bg-white flex justify-center gap-12 py-1 rounded-sm text-[25px] text-[#344151]">
+      <button onClick={decreaseHandler} className="border-r-2 text-center  w-20 broder-[#bfbfbf] text-5xl ">
+        -
+      </button>
+      <div className="font-bold gap-12 text-4xl">
+        {count}
+      </div>
+
+      <button onClick={increaseHandler}className="border-l-2 text-center  w-20 broder-[#bfbfbf] text-5xl" >
+        +
+      </button>
+
     </div>
+    <button onClick={resetHandler} className="m-7 bg-[#0398d4] text-white px-5 py2 rounded-sm text-lg">
+      Reset
+    </button>
+   </div>
   );
 }
 
